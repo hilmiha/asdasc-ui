6c545c059477f51486c2b2e19b028b36b6a92545
hilmiha <hilmihidayatar@gmail.com>
2025-10-05 18:25:17 +0700

    update github-page

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx'
 import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById('root')!).render(
-    <BrowserRouter>
+    <BrowserRouter basename="/asdasc-ui">
         <App/>
     </BrowserRouter>
 )
