ae2d89cadcfb9cd47c1af453a358c60907a85fe3
hilmiha <hilmihidayatar@gmail.com>
2025-10-17 19:47:48 +0700

    update github page

---
+++ docs/assets/index-_N_kcycG.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-Bw5SDHRi.js";const o=()=>e.jsx("div",{children:e.jsx("p",{children:"Generate Colors"})});export{o as default};
---
+++ docs/assets/preview-box-DTWTqvGV.js
@@ -0,0 +1,1 @@
+import{r as t,G as o,j as r}from"./index-Bw5SDHRi.js";const a=({children:e})=>{const{screenSize:s}=t.useContext(o);return r.jsx("div",{className:`preview-box ${s}`,children:e})};export{a as P};
